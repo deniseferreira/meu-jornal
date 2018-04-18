ca9fec25d1bf63c08b49e0be1739ae14719182b0
Denise <ferreiras.denise@hotmail.com>
2018-04-18 13:20:33 +0100

    ngIf

--- meu-jornal/src/app/noticias/noticias.component.ts
+++ meu-jornal/src/app/noticias/noticias.component.ts
@@ -7,11 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NoticiasComponent implements OnInit {
 
-noticias: string[] = ["Noticia 1", "Noticia 2", "Noticia 3", "Noticia 4"];
+  noticias: string[] = ["Noticia 1", "Noticia 2", "Noticia 3", "Noticia 4"];
+
+  mostrarNoticia: boolean = true;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public esconderNoticia(){
+    this.mostrarNoticia = false;
   }
 
 }
